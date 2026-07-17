ac3321d643853951066a33c2702adca8531c0214
PriyaRudroju <99352389+PriyaRudroju@users.noreply.github.com>
2026-07-16 19:31:18 -0600

    Update S3 bucket module source path

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 module "s3_bucket" {
-  source = "../../../modules/s3-bucket"
+  source = "../../modules/s-3bucket"
   bucket_name = var.bucket_name
   tags        = var.tags
 }
